f6dfee143317483767a991a850048104dac5dd05
Tati_MS <90469788+tatiana-sthl@users.noreply.github.com>
2022-06-19 15:49:21 +0200

    add js in view

--- js/view/column.js
+++ js/view/column.js
@@ -1,4 +1,5 @@
 import KanbanAPI from "../api/KanbanAPI.js";
+import Item from "./item.js";
 
 export default class Column {
     constructor(id, title) {
@@ -12,11 +13,13 @@ export default class Column {
         this.elements.title.textContent = title;
 
         this.elements.addItem.addEventListener("click", () => {
-            
+            const newItem = KanbanAPI.insertItem(id, "");
+
+            this.renderItem(newItem);
         });
 
         KanbanAPI.getItems(id).forEach(item => {
-            console.log(item);
+            this.renderItem(item);
         });
     }
 
@@ -32,5 +35,11 @@ export default class Column {
                 <button class="kanban__add-item" type="button">Add</button>
             </div>
         `).children[0];
+    }
+
+    renderItem(data) {
+        const item = new Item(data.id, data.content);
+
+        this.elements.items.appendChild(item.elements.root);
     }
 }
